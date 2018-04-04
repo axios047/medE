fc1bbf9411559c11099d9fe3061491de3c864c73
unknown <axios47@gmail.com>
2018-04-05 00:43:29 +0530

    add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { StartComponent } from './start/start.component';
+import { PnfComponent } from './pnf/pnf.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
+const appRoutes: Routes = [
+  { path: 'start', component: StartComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '',
+    redirectTo: '/start',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PnfComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    StartComponent,
+    PnfComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
